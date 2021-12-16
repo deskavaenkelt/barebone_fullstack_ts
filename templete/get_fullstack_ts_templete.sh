c87da5a6f9ae786f4ec54295eb44397c25e4e100
Lars Strömberg <stromberg.lars@gmail.com>
2021-12-16 21:13:21 +0100

    templete file

--- templete/get_fullstack_ts_templete.sh
+++ templete/get_fullstack_ts_templete.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
+# Variabels
+SERVER_FOLDER="server"
+CLIENT_FOLDER="client"
+
 
 # Create project folder based on project name
 read -p 'Project name: ' PROJECT_NAME
-mkdir "$PROJECT_NAME"
+mkdir "$PROJECT_NAME" && cd "$PROJECT_NAME" || exit
+
+# Create server folder
+cd "$SERVER_FOLDER" || exit
+mkdir src && cd src || exit
 
 # Download template server
-
-
+# server
+# +-
+URL_TEMPLATE_SERVER="https://raw.githubusercontent.com/deskavaenkelt/barebone_fullstack_ts/main/templete/server"
+wget "$URL_TEMPLATE_SERVER"/src/server.ts
+cd ..
+wget "$URL_TEMPLATE_SERVER"/Dockerfile
+wget "$URL_TEMPLATE_SERVER"/package.json
+wget "$URL_TEMPLATE_SERVER"/README.md
+wget "$URL_TEMPLATE_SERVER"/tsconfig.json
 
 #rm -rf server
 #SERVER_FOLDER="server"
